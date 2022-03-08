ab4a4e8963e259d260f9663c8982be5301ecf1b2
TzuYuOu <a1225johnny@gmail.com>
2022-03-08 21:02:06 +0800

    Update media url

--- dataQuality/urls.py
+++ dataQuality/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +24,5 @@ urlpatterns = [
     path('members/',include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
     path('mainapp/', include('mainapp.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
