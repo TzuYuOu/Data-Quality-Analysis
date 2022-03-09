bccea2262fc71a8ecfc56d8880c5b284e2977f8c
TzuYuOu <a1225johnny@gmail.com>
2022-03-09 13:02:09 +0800

    Update staticfiles setting

--- dataQuality/settings.py
+++ dataQuality/settings.py
@@ -138,7 +138,7 @@ STATIC_URL =  '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # new
 MEDIA_URL = '/files/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/files')
@@ -151,4 +151,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
